fecec5ef8d3823c99b2be21017c27ddbd5810aa2
Hussnain Quresshi <hussnain77133@gmail.com>
2020-07-08 00:15:56 +0500

    first commit

---
+++ rate-limit.js
@@ -0,0 +1,73 @@
+'use strict';
+module.exports = function createRateLimitChecker(CORSANYWHERE_RATELIMIT) {
+  var rateLimitConfig = /^(\d+) (\d+)(?:\s*$|\s+(.+)$)/.exec(
+    CORSANYWHERE_RATELIMIT
+  );
+  if (!rateLimitConfig) {
+    return function checkRateLimit() {};
+  }
+  var maxRequestsPerPeriod = parseInt(rateLimitConfig[1]);
+  var periodInMinutes = parseInt(rateLimitConfig[2]);
+  var unlimitedPattern = rateLimitConfig[3];
+  if (unlimitedPattern) {
+    var unlimitedPatternParts = [];
+    unlimitedPattern
+      .trim()
+      .split(/\s+/)
+      .forEach(function (unlimitedHost, i) {
+        var startsWithSlash = unlimitedHost.charAt(0) === '/';
+        var endsWithSlash = unlimitedHost.slice(-1) === '/';
+        if (startsWithSlash || endsWithSlash) {
+          if (
+            unlimitedHost.length === 1 ||
+            !startsWithSlash ||
+            !endsWithSlash
+          ) {
+            throw new Error(
+              'Invalid CORSANYWHERE_RATELIMIT. Regex at index ' +
+                i +
+                ' must start and end with a slash ("/").'
+            );
+          }
+          unlimitedHost = unlimitedHost.slice(1, -1);
+
+          new RegExp(unlimitedHost);
+        } else {
+          unlimitedHost = unlimitedHost.replace(/[$()*+.?[\\\]^{|}]/g, '\\$&');
+        }
+        unlimitedPatternParts.push(unlimitedHost);
+      });
+    unlimitedPattern = new RegExp(
+      '^(?:' + unlimitedPatternParts.join('|') + ')$',
+      'i'
+    );
+  }
+
+  var accessedHosts = Object.create(null);
+  setInterval(function () {
+    accessedHosts = Object.create(null);
+  }, periodInMinutes * 60000);
+
+  var rateLimitMessage =
+    'The number of requests is limited to ' +
+    maxRequestsPerPeriod +
+    (periodInMinutes === 1
+      ? ' per minute'
+      : ' per ' + periodInMinutes + ' minutes') +
+    '. ' +
+    'Please self-host CORS Anywhere if you need more quota. ' +
+    'See https://github.com/HussnainQuresshi/corsbypass';
+
+  return function checkRateLimit(origin) {
+    var host = origin.replace(/^[\w\-]+:\/\//i, '');
+    if (unlimitedPattern && unlimitedPattern.test(host)) {
+      return;
+    }
+    var count = accessedHosts[host] || 0;
+    ++count;
+    if (count > maxRequestsPerPeriod) {
+      return rateLimitMessage;
+    }
+    accessedHosts[host] = count;
+  };
+};
---
+++ server.js
@@ -0,0 +1,36 @@
+var host = process.env.HOST || '0.0.0.0';
+var port = process.env.PORT || 8080;
+var originBlacklist = parseEnvList(process.env.CORSANYWHERE_BLACKLIST);
+var originWhitelist = parseEnvList(process.env.CORSANYWHERE_WHITELIST);
+function parseEnvList(env) {
+  if (!env) {
+    return [];
+  }
+  return env.split(',');
+}
+var checkRateLimit = require('./rate-limit')(
+  process.env.CORSANYWHERE_RATELIMIT
+);
+var cors_proxy = require('./corsbypass');
+cors_proxy
+  .createServer({
+    originBlacklist: originBlacklist,
+    originWhitelist: originWhitelist,
+    requireHeader: ['origin', 'x-requested-with'],
+    checkRateLimit: checkRateLimit,
+    removeHeaders: [
+      'cookie',
+      'cookie2',
+      'x-heroku-queue-wait-time',
+      'x-heroku-queue-depth',
+      'x-heroku-dynos-in-use',
+      'x-request-start',
+    ],
+    redirectSameOrigin: true,
+    httpProxyOptions: {
+      xfwd: false,
+    },
+  })
+  .listen(port, host, function () {
+    console.log('Running CORSBYPASS on ' + host + ':' + port);
+  });
